b534dba1253368562e8136eba230daa135a3caf9
Shadow-xd0 <78281397+Shadow-xd0@users.noreply.github.com>
2024-04-17 12:31:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "+595982634142"
 global.confirmCode = ""
 
 global.owner = [
